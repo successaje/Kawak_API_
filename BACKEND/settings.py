b4342f9769b584a163900bf1738bf6536d193749
Success Aje <73680210+successaje@users.noreply.github.com>
2022-03-22 14:38:31 +0100

    Update settings.py

--- BACKEND/settings.py
+++ BACKEND/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "eko5y90dv=o)+-h-#hlk(is4&bbsq#1au(*f*kmgyd618d_mg4"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
